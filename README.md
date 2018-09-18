d82fd137f291b78d41db1a74186f5cd501869e68
Sienbo <sienbo1992@163.com>
2018-09-18 21:20:41 +0800

    增加一个简单的线性回归类，并增加一个data2.txt数据

--- README.md
+++ README.md
@@ -2,8 +2,13 @@
 ## INTRO：
   - linearRegression
     - 线性回归
-    - TAG：监督学习，回归算法，线性算法
-    - 优化算法：1）最小二乘法；2）梯度下降法
+	    - TAG：监督学习，回归算法，线性算法
+	    - 优化算法：1）最小二乘法；2）梯度下降法
+    - 文件夹中包含了以下文件：LinearRegression.py;LinearRegression_least_square_method.py;LinearRegression_scikit-learn.py;data.txt;data2.txt
+		- LinearRegression.py:实现了一个简单的线性回归类，优化算法使用梯度下降法，模型训练完后，可以查看模型参数、模型训练中的损失值、一元特征样本的样本与模型比照图；还有用于预测的predict类方法。
+		- LinearRegression_least_square_method.py：使用最小二乘法实现一遍线性回归，这里时使用函数型式，并没有创建类。
+		- LinearRegression_scikit-learn.py：使用sklearnz库中现有的模型模拟了一边其中线性回归类的使用方法。
+		- data.txt、data2.txt：自己随意创建的样本集，data样本中只有一个特征、data2样本中又两个特征。
   - logisticRegression
     - 逻辑回归
     - TAG：监督学习，分类算法，线性算法
--- linearRegression/LinearRegression.py
+++ linearRegression/LinearRegression.py
@@ -1,79 +1,83 @@
 #-*- coding: utf-8 -*-
 import numpy as np
 from matplotlib import pyplot as plt
-from matplotlib.font_manager import FontProperties
 
-def linearRegression(alpha=0.1,num_iters=400):
-    print("加载数据\n···\n···")
-    
-    data = loadtxtAndcsv_data("data.txt",",",np.float64)  #读取数据
-    X = data[:,:-1]      # X对应0到倒数第2列                  
-    y = data[:,-1]        # y对应最后一列  
-    m = data.shape[0]         # 总的数据条数
-    col = data.shape[1]      # data的列数
-    X = featureNormaliza(X)    # 归一化
-    
-    print("执行梯度下降算法....\n")
-    
-    theta = np.zeros(col)
-    theta,J_history = gradientDescent(X, y, theta, alpha, num_iters)
-    plot_fit(X,y,theta)         # 画图看一下效果
-    plotJ(J_history, num_iters)
-    
-# 加载txt和csv文件
-def loadtxtAndcsv_data(fileName,split,dataType):
-    return np.loadtxt(fileName,delimiter=split,dtype=dataType)
+class LinearRegression():
+    '''
+    这是一个线性回归类
+    优化算法使用梯度下降法
+    使用实例：
+        lr = LinearRegression() #实例化类
+        lr.fit(X_train,y_train) #训练模型
+        y_predict = lr.predict(X_test) #预测训练数据
+        lr.plotFigure()用于画出样本散点图与预测模型（仅限于样本特征为1时）
+    '''
+    def __init__(self,alpha=0.02,n_iter=1000):
+        self._alpha = alpha
+        self._niter = n_iter
 
-# 加载npy文件
-def loadnpy_data(fileName):
-    return np.load(fileName)
+    #使用0初始化参数
+    def initialPara(self,n):
+        if n == 1:
+            return 0,0
+        else:
+            return np.zeros(n),0
 
-# 归一化feature
-def featureNormaliza(X):
-    mu = np.mean(X,0)          # 求每一列的平均值（0指定为列，1代表行）
-    sigma = np.std(X,0)        # 求每一列的标准差
-    for i in range(X.shape[1]):     # 遍历列
-       X[:,i] = (X[:,i]-mu[i])/sigma[i]  # 归一化
-    return X
+    #训练工程
+    def fit(self,X_train,y_train):
+        #保存原始数据
+        self.X_source = X_train.copy()
+        self.y_source = y_train.copy()
+        # 初始化w，w0
+        sample_num = X_train.shape[0]
+        if len(X_train.shape) == 1:
+            self._w, self._w0 = self.initialPara(1)
+        else:
+            self._w,self._w0 = self.initialPara(X_train.shape[1])
+        #创建列表存放每次每次迭代后的偏差值
+        self.cost = []
+        #开始迭代
+        for _ in range(self._niter):
+            y_predict = self.predict(X_train)
+            y_bias = y_train - y_predict
+            self.cost.append(np.dot(y_bias,y_bias)/(2 * sample_num))
+            self._w += self._alpha * np.dot(X_train.T,y_bias)/sample_num
+            self._w0 += self._alpha * np.sum(y_bias)/sample_num
 
-# 画二维图
-def plot_fit(x,y,theta):
-    plt.scatter(x,y,color='r',s=10)
-    x1 = np.arange(-2,2,0.01)
-    y1 = np.dot(x1,theta[1]) + theta[0]
-    plt.plot(x1,y1,color='b')
-    plt.show()
-    print('linear_regression is: ',str(theta[0]),' + ',str(theta[1]),'* x')
+    def predict(self,X_test):
+        if len(X_test.shape) == 1:
+            return self._w * X_test + self._w0
+        else:
+            return X_test.dot(self._w) + self._w0
+
+    #画出样本散点图以及使用模型预测的线条
+    def plotFigure(self):
+        #画出样本散点图
+        plt.scatter(self.X_source,self.y_source)
+
+        #画模型图
+        x1_min = self.X_source.min()
+        x1_max = self.X_source.max()
+        X_predict = np.arange(x1_min,x1_max,step=0.01)
+        #画出预测图形
+        plt.plot(X_predict,self._w*X_predict+self._w0)
+        plt.show()
 
 
-# 梯度下降算法
-def gradientDescent(X,y,theta,alpha,num_iters):
-    m = len(y)      
-    n = len(theta)
-    J_history = []
-    
-    for i in range(num_iters):  # 遍历迭代次数    
-        y_predict = np.dot(X,theta[1:]) + theta[0]     # 计算预测输出
-        errors = y_predict - y 						   # 预测值和实际值之间的差
-        cost_value = np.dot(errors,errors)/(2*m)	   # 代价值
-        theta[1:] -= alpha * np.dot(X.T,errors)/m 				   #梯度的计算
-        theta[0] -= alpha * np.sum(errors)/m
-        J_history.append(cost_value)      #添加代价值
-    #print(J_history)
-    return theta,J_history  
-
-# 画每次迭代代价的变化图
-def plotJ(J_history,num_iters):
-    x = np.arange(1,num_iters+1)
-    plt.plot(x,J_history)
-    plt.xlabel(r"n_iers") # 注意指定字体，要不然出现乱码问题
-    plt.ylabel(r"cost_value")
-    plt.title(r"linearRegression",)
-    plt.show()
-
-# 测试linearRegression函数
-def testLinearRegression():
-    linearRegression(0.5,1000)
-    
 if __name__ == "__main__":
-    testLinearRegression()
+    import pandas as pd
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    df = pd.read_csv("data.txt",header=None)
+    X_train = df.iloc[:,0].values
+    y_train = df.iloc[:,1].values
+
+    lr = LinearRegression()
+    lr.fit(X_train,y_train)
+    #打印出参数
+    print(lr._w,lr._w0)
+    #画出损失值随迭代次数的变化图
+    plt.plot(lr.cost)
+    plt.show()
+    lr.plotFigure()
