ace90b6f2b4cfe5fc6e32e93ca0a67fc3b8d1b31
Sienbo <sienbo1992@163.com>
2018-09-22 14:41:54 +0800

    python实现的线性回归类

--- linearRegression/LinearRegression.py
+++ linearRegression/LinearRegression.py
@@ -13,31 +13,35 @@ class LinearRegression():
         lr.plotFigure()用于画出样本散点图与预测模型（仅限于样本特征为1时）
     '''
     def __init__(self,alpha=0.02,n_iter=1000):
-        self._alpha = alpha
-        self._niter = n_iter
+        self._alpha = alpha     #步长
+        self._n_iter = n_iter    #最大迭代次数
 
-    #使用0初始化参数
+    #初始化模型参数
     def initialPara(self,n):
+        # 只有一个特征时
         if n == 1:
             return 0,0
         else:
             return np.zeros(n),0
 
-    #训练工程
+    #训练模型
     def fit(self,X_train,y_train):
         #保存原始数据
         self.X_source = X_train.copy()
         self.y_source = y_train.copy()
+
         # 初始化w，w0
         sample_num = X_train.shape[0]
         if len(X_train.shape) == 1:
             self._w, self._w0 = self.initialPara(1)
         else:
             self._w,self._w0 = self.initialPara(X_train.shape[1])
+
         #创建列表存放每次每次迭代后的偏差值
         self.cost = []
-        #开始迭代
-        for _ in range(self._niter):
+
+        #开始训练迭代
+        for _ in range(self._n_iter):
             y_predict = self.predict(X_train)
             y_bias = y_train - y_predict
             self.cost.append(np.dot(y_bias,y_bias)/(2 * sample_num))
@@ -53,12 +57,13 @@ class LinearRegression():
     #画出样本散点图以及使用模型预测的线条
     def plotFigure(self):
         #画出样本散点图
-        plt.scatter(self.X_source,self.y_source)
+        plt.scatter(self.X_source,self.y_source,c='r',label="samples",linewidths=0.4)
 
         #画模型图
         x1_min = self.X_source.min()
         x1_max = self.X_source.max()
         X_predict = np.arange(x1_min,x1_max,step=0.01)
+        plt.legend(loc='upper left')
         #画出预测图形
         plt.plot(X_predict,self._w*X_predict+self._w0)
         plt.show()
@@ -66,13 +71,13 @@ class LinearRegression():
 
 if __name__ == "__main__":
     import pandas as pd
-    import numpy as np
-    import matplotlib.pyplot as plt
 
-    df = pd.read_csv("data.txt",header=None)
+    #读取样本文件数据
+    df = pd.read_csv("data.txt")
     X_train = df.iloc[:,0].values
     y_train = df.iloc[:,1].values
 
+    #使用线性回归类生成模型
     lr = LinearRegression()
     lr.fit(X_train,y_train)
     #打印出参数
@@ -80,4 +85,9 @@ if __name__ == "__main__":
     #画出损失值随迭代次数的变化图
     plt.plot(lr.cost)
     plt.show()
+    #画出样本散点图以及模型的预测图
     lr.plotFigure()
+
+    #预测x
+    x = np.array([3])
+    print("The input x is{0},then the predict of y is:{1}".format(x,lr.predict(x)))
